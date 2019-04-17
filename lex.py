a7e85faf47d25346533e788f17a04a54d6380915
juy13 <49489282+juy13@users.noreply.github.com>
2019-04-17 22:11:50 +0300

    Add files via upload

--- lex.py
+++ lex.py
@@ -15,31 +15,59 @@ def log(came_log):
 
 reserved = {
 	 'TRUE' 		: 'true',
-	 'FALSE' 		: 'false', 
+	 'FALSE' 		: 'false',
+	 
 	 'LOGIC' 		: 'bool',
 	 'UNDEF' 		: 'var',
 	 'NUMERIC' 		: 'int',
 	 'STRING' 		: 'string',
 	 'RECORD' 		: 'stuct',
+	 
 	 'CONVERSION' 	: 'convert',
 	 'TO' 			: 'to',
 	 'FROM' 		: 'from',
+	 
 	 'BLOCK' 		: 'start',
 	 'UNBLOCK' 		: 'end',
+	 
 	 'PROC' 		: 'def',
+	 
 	 'MOVEUP' 		: 'up',
 	 'MOVEDOWN' 	: 'down',
 	 'MOVELEFT' 	: 'left',
 	 'MOVERIGHT' 	: 'right',
+	 
 	 'PINGUP' 		: 'knockup',
 	 'PINGDOWN'		: 'knockdown',
 	 'PINGRIGHT' 	: 'knockright',
 	 'PINGLEFT' 	: 'knockleft',
+	 
 	 'VISION' 		: 'vision',
 	 'VOICE' 		: 'input'
 	}
 	
-tokens = ['ID'] + list(reserved.values())
+tokens = ['ID', 'QUOTE', 'LBRACKET', 'RBRACKET', 'LBRACE', 'RBRACE', 
+		'EQ', 'PLUS', 'MINUS', 'TIMES', 'POWER', 'DIVIDE', 'LT',
+		'GT', 'QM', 'EX', 'AND', 'DEQ'] + list(reserved.values())
+
+t_LQUOTE = '\"'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+t_DEQ = r'=='
+t_EQ = r'='
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_POWER = r'\^'
+t_DIVIDE = r'/'
+t_LT = r'<'
+t_GT = r'>'
+t_QM = r'\?'
+t_EX = r'\!'
+t_AND = r'\&'
+#t_STRING = r'\".*?\"'
 
 def t_ID(t):
      r'[a-zA-Z_][a-zA-Z_0-9]*'
@@ -54,7 +82,7 @@ def t_newline(t):
 	
 	 
 def t_error(t):
-	log("Illegal character '%s'" % t.value[0])	 
+	print("Illegal character '%s' at line '%s'" % (t.value[0], t.lexer.lineno))	 
 
 t_ignore = ' \r\t\f'
 	
